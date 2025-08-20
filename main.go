56534d52550f8b3273f6140a98028c98e073ac34
ElevenNotes <github@11notes.ch>
2025-08-20 22:14:20 +0200

    feat: global output on --bye

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 
 const UPX string = "/usr/local/bin/upx"
 const DS string = "/usr/local/bin/ds"
+const ENV string = "DS_UPX_TOTAL_SAVINGS"
 
 func prettyByteSize(b int) string {
 	bf := float64(b)
@@ -24,12 +25,21 @@ func prettyByteSize(b int) string {
 	return fmt.Sprintf("%.1fYiB", bf)
 }
 
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+			return value
+	}
+	return fallback
+}
+
 func main(){
 	if(len(os.Args) > 1){
 		args := os.Args[1:]
 		if len(args) > 0 {
 			file := args[0]
 			if file == "--bye" {
+				bytesTotal, _ := strconv.Atoi(getEnv(ENV, "0"))
+				fmt.Fprintf(os.Stdout, "DS TOTAL SAVINGS: %s\n", prettyByteSize(bytesTotal))
 				os.Remove(UPX)
 				os.Remove(DS)
 			}else{
@@ -42,7 +52,9 @@ func main(){
 						if len(matches[0]) > 2 {
 							bytesBefore, _ := strconv.Atoi(matches[0][1])
 							bytesAfter, _ := strconv.Atoi(matches[0][2])
+							bytesTotal, _ := strconv.Atoi(getEnv(ENV, "0"))
 							fmt.Fprintf(os.Stdout, "file %s shrunk by %s (~%s)\n", file, matches[0][3], prettyByteSize(bytesBefore - bytesAfter))
+							os.Setenv(ENV, strconv.Itoa(bytesTotal + (bytesBefore - bytesAfter)))
 						}
 					}
 				}
