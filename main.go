391d9a5174d5e389aac35f41776cec849ac982c8
ElevenNotes <github@11notes.ch>
2025-08-20 20:46:04 +0200

    feat: self destruct

--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 	"math"
 )
 
+const UPX string = "/usr/local/bin/upx"
+const DS string = "/usr/local/bin/ds"
+
 func prettyByteSize(b int) string {
 	bf := float64(b)
 	for _, unit := range []string{"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"} {
@@ -26,16 +29,21 @@ func main(){
 		args := os.Args[1:]
 		if len(args) > 0 {
 			file := args[0]
-			cmd := exec.Command("/usr/local/bin/upx", "-q", "--no-backup", "-9", "--best", "--lzma", file)
-			cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid:true}
-			stdout, err := cmd.Output()
-			if err == nil {
-				matches := regexp.MustCompile(`(\d+)\s+->\s+(\d+)\s+(\S+)`).FindAllStringSubmatch(string(stdout), -1)
-				if len(matches) > 0 {
-					if len(matches[0]) > 2 {
-						bytesBefore, _ := strconv.Atoi(matches[0][1])
-						bytesAfter, _ := strconv.Atoi(matches[0][2])
-						fmt.Fprintf(os.Stdout, "file %s shrunk by %s (~%s)\n", file, matches[0][3], prettyByteSize(bytesBefore - bytesAfter))
+			if file == "--bye" {
+				os.Remove(UPX)
+				os.Remove(DS)
+			}else{
+				cmd := exec.Command(UPX, "-q", "--no-backup", "-9", "--best", "--lzma", file)
+				cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid:true}
+				stdout, err := cmd.Output()
+				if err == nil {
+					matches := regexp.MustCompile(`(\d+)\s+->\s+(\d+)\s+(\S+)`).FindAllStringSubmatch(string(stdout), -1)
+					if len(matches) > 0 {
+						if len(matches[0]) > 2 {
+							bytesBefore, _ := strconv.Atoi(matches[0][1])
+							bytesAfter, _ := strconv.Atoi(matches[0][2])
+							fmt.Fprintf(os.Stdout, "file %s shrunk by %s (~%s)\n", file, matches[0][3], prettyByteSize(bytesBefore - bytesAfter))
+						}
 					}
 				}
 			}
