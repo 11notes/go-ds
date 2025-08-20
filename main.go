246f362535e9021c2b5bfb434863ef14eac9a5aa
ElevenNotes <github@11notes.ch>
2025-08-20 23:41:45 +0200

    feat: store total as temp file

--- main.go
+++ main.go
@@ -8,11 +8,12 @@ import (
 	"regexp"
 	"strconv"
 	"math"
+	"io/ioutil"
 )
 
 const UPX string = "/usr/local/bin/upx"
 const DS string = "/usr/local/bin/ds"
-const ENV string = "DS_UPX_TOTAL_SAVINGS"
+const STORE string = "/tmp/.ds"
 
 func prettyByteSize(b int) string {
 	bf := float64(b)
@@ -25,11 +26,28 @@ func prettyByteSize(b int) string {
 	return fmt.Sprintf("%.1fYiB", bf)
 }
 
-func getEnv(key, fallback string) string {
-	if value, ok := os.LookupEnv(key); ok {
-			return value
+func getTotal() int {
+	data, err := ioutil.ReadFile(STORE)
+	if err != nil{
+		return 0
 	}
-	return fallback
+	total, _ := strconv.Atoi(string(data))
+	if err != nil {
+		return 0
+	}
+	return total
+}
+
+func addTotal(b int) {
+	total := 0
+	if _, err := os.Stat(STORE); err == nil {
+		data, err := ioutil.ReadFile(STORE)
+		if err == nil {
+			total, _ = strconv.Atoi(string(data))
+		}
+	}
+	total += b
+	ioutil.WriteFile(STORE, []byte(strconv.Itoa(total)), os.ModePerm)
 }
 
 func main(){
@@ -38,10 +56,10 @@ func main(){
 		if len(args) > 0 {
 			file := args[0]
 			if file == "--bye" {
-				bytesTotal, _ := strconv.Atoi(getEnv(ENV, "0"))
-				fmt.Fprintf(os.Stdout, "DS TOTAL SAVINGS: %s\n", prettyByteSize(bytesTotal))
+				fmt.Fprintf(os.Stdout, "DS TOTAL SAVINGS: %s\n", prettyByteSize(getTotal()))
 				os.Remove(UPX)
 				os.Remove(DS)
+				os.Remove(STORE)
 			}else{
 				if file != UPX && file != DS {
 					cmd := exec.Command(UPX, "-q", "--no-backup", "-9", "--best", "--lzma", file)
@@ -53,9 +71,8 @@ func main(){
 							if len(matches[0]) > 2 {
 								bytesBefore, _ := strconv.Atoi(matches[0][1])
 								bytesAfter, _ := strconv.Atoi(matches[0][2])
-								bytesTotal, _ := strconv.Atoi(getEnv(ENV, "0"))
 								fmt.Fprintf(os.Stdout, "file %s shrunk by %s (~%s)\n", file, matches[0][3], prettyByteSize(bytesBefore - bytesAfter))
-								os.Setenv(ENV, strconv.Itoa(bytesTotal + (bytesBefore - bytesAfter)))
+								addTotal(bytesBefore - bytesAfter)
 							}
 						}
 					}
